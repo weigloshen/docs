e96ca8fadda60019f28dba7fe9fb2fd2cb720087
wss <weishusheng100@gmail.com>
2023-03-10 12:28:29 +0800

    添加algolia搜索

--- docs/.vitepress/editLink.ts
+++ docs/.vitepress/editLink.ts
@@ -1,4 +1,4 @@
 export const editLink = {
-  pattern: "https://github.com/ZhouYue8/myvitepress/tree/main/docs/:path",
+  pattern: "https://github.com/ZhouYue8/docs/tree/main/docs/:path",
   text: "Edit this page on GitHub",
 };
